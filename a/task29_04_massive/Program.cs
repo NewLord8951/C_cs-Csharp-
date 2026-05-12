6c95a9abb9b10979235bab8f3d65dd68de8c7d06
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-05-12 11:28:14 +0300

    Не рпботает

---
+++ a/task29_04_massive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+Console.Write("Введите размер массива: ");
+int a = int.Parse(Console.ReadLine());
+int[] h = new int[a];
+for(int i = 0; i < a; i++) {
+    Console.Write("Введите элемент массива: ");
+    h[i] = int.Parse(Console.ReadLine());
+}
+for(int i = 1; i < a - 1; i++) {
+    if(i == 2) {
+        if(h[i] < h[i + 1]) {
+            Console.Write("ЫA: ");
+            Console.WriteLine(h[i]);
+        }
+    }
+    if(h[i] < h[i - 1] && h[i] < h[i + 1]) {
+        Console.Write("Ы: ");
+        Console.WriteLine(h[i]);
+    }
+    if(i == a) {
+        if(h[i] < h[i - 1]) {
+            Console.Write("ЫS: ");
+            Console.WriteLine(h[i]);
+        }
+    }
+}
