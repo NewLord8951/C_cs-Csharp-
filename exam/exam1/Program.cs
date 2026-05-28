822ed7d56b47033982b3fa5d3326a666cbaffa8f
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-05-28 10:42:43 +0300

    П

---
+++ exam/exam1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Person
+{
+    protected string name;
+    protected Person(string name)
+    {
+        this.name = name;
+    }
+    public virtual void introduce()
+    {
+        Console.Write("Меня зовут " + name);
+    }
+}
+
+class Student : Person
+{
+    private string guard;
+    public Student(string name, string guard) : base(name)
+    {
+        this.guard = guard;
+    }
+    public override void introduce()
+    {
+        Console.Write("Меня зовут " + name + ", я учусь в " + guard + " классе");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student a = new Student("Леонид", "-1");
+        a.introduce();
+    }
+}
