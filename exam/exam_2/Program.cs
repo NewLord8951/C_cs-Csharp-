eaf0ceae198c1ba27ed0f24a47de78af1c0751f4
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-06-24 09:44:36 +0300

    Ы

---
+++ exam/exam_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void Speak() {}
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        base.Speak();
+        Console.WriteLine("Гав");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+        dog.Speak(); 
+    }
+}
